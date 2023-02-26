ef27cee1b1fec7ccc00bb98ea364aaad5f96ebd3
Azimbek Toktosunov <94052030+azimust@users.noreply.github.com>
2023-02-26 16:18:03 +0600

    Delete 10-day directory

--- 10-day/double-sort.js
+++
@@ -1,7 +0,0 @@
-function dbSort(a){
-    // Code here
-    let word = a.filter((v) => typeof v === 'string').sort();
-    let number = a.filter((v) => typeof v === 'number').sort((a, b) => a - b);
-    return number.concat(word);
-}
-console.log(dbSort(["Banana", "Orange", "Apple", "Mango", 3, 2, 2]));
--- 10-day/zip-with.js
+++
@@ -1,9 +0,0 @@
-function zipWith(fn,a0,a1) {
-    let arr = [];
-    let short = Math.min(a0.length, a1.length);
-    for(let i = 0; i < short; i++){
-        arr.push(fn(a0[i], a1[i]));
-    }
-    return arr;
-}
-console.log(zipWith(Math.pow, [10,10,10,10], [0,1,2,3]));
