c49ae3ca417c75114c6ce8164e86f6c9df4ee2dc
Azimbek Toktosunov <94052030+azimust@users.noreply.github.com>
2023-05-05 20:40:48 +0600

    feat: add 76-day files

---
+++ #76-day/twice-ad-old.js
@@ -0,0 +1,5 @@
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  // your code here
+    return Math.abs(dadYearsOld - (sonYearsOld * 2))
+}
+console.log(twiceAsOld(55, 30));
---
+++ #76-day/wilson-primes.js
@@ -0,0 +1,4 @@
+function amIWilson(p) {
+    return p==5 || p==13 || p==563
+}
+console.log(amIWilson(5));
